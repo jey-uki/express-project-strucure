869ce57a51298c72c78c37ccc54421323d586ca2
jey-uki <srjey.uki@gmail.com>
2025-09-29 14:40:32 +0530

    user route created using express Route()

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import express from 'express'
 
 const app = express()
+const router = express.Router();
+
 const PORT = 3000;
 
 // HTTP method handling
@@ -9,16 +11,11 @@ app.get('/', (req, res) => {
     res.send('Hello Express!');
 });
 
-app.route('/book')
-    .get((req, res) => {
-        res.send('Get a random book')
-    })
-    .post((req, res) => {
-        res.send('Add a book')
-    })
-    .put((req, res) => {
-        res.send('Update the book')
-    })
+router.get('/user', (req, res) => {
+    res.send('This is GET user...')
+})
+
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
